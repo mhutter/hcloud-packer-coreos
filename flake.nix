c97edb8f006d942e818abf24516b8cc5b38915fa
Manuel Hutter <mhutter@users.noreply.github.com>
2024-04-17 09:07:40 +0200

    Bump toolchian versions (#15)
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
     in
     {
       devShells."${system}".default = pkgs.mkShell
